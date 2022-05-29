e16a54951b7173b892ece1bef15d6507f7d8caf1
Evgene Popolamov <eugene.popolamov@effective-soft.com>
2022-05-29 22:43:27 +0300

    feat: add morgan and cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
-const port = 5000;
+import mogoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan';
 
+// 123dev - password
+// mongodb+srv://Evgene:<password>@cluster0.uzhho.mongodb.net/?retryWrites=true&w=majority
 const app = express();
+
+const port = 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello express')
 });
 
 app.listen(port, () => {
-  console.log(`server running on port: ${port}`);
+  console.log(`server is running on port: ${port}`);
 })
