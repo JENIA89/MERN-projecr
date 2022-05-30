f636235c96ce828b4ae7cda84fbec824a86c8615
Evgene Popolamov <eugene.popolamov@effective-soft.com>
2022-05-30 17:11:30 +0300

    feat: run server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
 import express from 'express';
-import mogoose from 'mongoose';
 import cors from 'cors';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 // 123dev - password
 // mongodb+srv://Evgene:<password>@cluster0.uzhho.mongodb.net/?retryWrites=true&w=majority
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.json({limit: '30mb', extended: true}));
+app.use(express.urlencoded({limit: '30mb', extended: true}));
+app.use(cors());
+
+const MONGODB_URL = 'mongodb+srv://Evgene:123dev@cluster0.uzhho.mongodb.net/tour_db?retryWrites=true&w=majority'
+
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello express')
-});
-
-app.listen(port, () => {
-  console.log(`server is running on port: ${port}`);
-})
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    app.listen(port, () => console.log(`server is running on port: ${port}`));
+  })
+  .catch(err => console.log(`${err} did not connect`));
