432edae487724e43c402a2a4ee17ba82b60b4e29
Eugene <eugene.popolamov@effective-soft.com>
2022-06-15 12:35:26 +0300

    feat: add tour middleware

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -2,4 +2,12 @@ import axios from "axios";
 
 export const baseURL = process.env.REACT_APP_BASE_API;
 
-export const api = axios.create({baseURL})
+export const api = axios.create({baseURL});
+
+api.interceptors.request.use(req => {
+  if(localStorage.getItem('profile')) {
+    // @ts-ignore
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+  }
+  return req;
+})
--- server/routes/tour.js
+++ server/routes/tour.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { createTour, getTour } from "../controllers/tour.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
-router.post("/", createTour);
+router.post("/", auth, createTour);
 router.get("/", getTour);
 
 
